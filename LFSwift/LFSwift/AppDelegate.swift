dbc17722b10801407e7da25ae3ce3c662fec7246
tianwanli <tianwangli@kangmei.com.cn>
2019-01-04 17:25:22 +0800

    添加carthage

--- LFSwift/LFSwift/AppDelegate.swift
+++ LFSwift/LFSwift/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MonkeyKing
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+//        MonkeyKing.registerAccount(.weChat(appID: <#T##String#>, appKey: <#T##String?#>, miniAppID: <#T##String?#>))
         return true
     }
 
