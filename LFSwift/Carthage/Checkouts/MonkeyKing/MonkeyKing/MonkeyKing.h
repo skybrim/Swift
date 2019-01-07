91f9e1cd6ec57200e1d6afa2d0a27b4e8966ab48
tianwanli <tianwangli@kangmei.com.cn>
2019-01-07 16:58:31 +0800

    删除旧工程

--- LFSwift/Carthage/Checkouts/MonkeyKing/MonkeyKing/MonkeyKing.h
+++
@@ -1,8 +0,0 @@
-
-#import <UIKit/UIKit.h>
-
-//! Project version number for MonkeyKing.
-FOUNDATION_EXPORT double MonkeyKingVersionNumber;
-
-//! Project version string for MonkeyKing.
-FOUNDATION_EXPORT const unsigned char MonkeyKingVersionString[];
