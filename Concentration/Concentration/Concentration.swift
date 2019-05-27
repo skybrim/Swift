cb861d39469af7dd49b4cdab43bb0c9475b3ec8e
Wiley <throughskybrim@gmail.com>
2019-05-27 16:44:56 +0800

    洗牌算法

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -47,6 +47,7 @@ class Concentration {
     //洗牌
     func shuffCards()  {
        
+    
     }
     
     init(numberOfPairsOfCards: Int) {
@@ -58,5 +59,27 @@ class Concentration {
         }
         
         //TODO: Shuffle the cards
+        cards.shuffle() //直接调用系统的方法
+        /*
+         Fisher-Yates Shuffle算法
+         1. 初始化原始数组和新数组，原始数组长度为n(已知)；
+         2. 从还没处理的数组（假如还剩k个）中，随机产生一个[0, k)之间的数字p（假设数组从0开始）；
+         3. 从剩下的k个数中把第p个数取出；
+         4. 重复步骤2和3直到数字全部取完；
+         5. 从步骤3取出的数字序列便是一个打乱了的数列。
+         */
+        
+        /*
+         经典洗牌算法
+         Knuth-Durstenfeld Shuffle，不能计算自增长的数组，会改变原属组
+         1. 建立一个数组大小为 n 的数组 arr，分别存放 1 到 n 的数值；
+         2. 生成一个从 0 到 n - 1 的随机数
+         3. 输出 arr 下标为 x 的数值，即为第一个随机
+         4. 将 arr 的尾元素和下标为 x 的元素
+         5. 同2，生成一个从 0 到 n - 2 的随机数
+         6. 输出 arr 下标为 x 的数值，为第二个随机
+         7. 将 arr 的倒数第二个元素和下标为 x 的
+         如上，直到输出 m 个数为止
+         */
     }
 }
