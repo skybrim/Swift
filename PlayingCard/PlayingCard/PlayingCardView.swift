809aec6851454a8f9d14b6d169610976c3138d41
Wiley <throughskybrim@gmail.com>
2019-06-02 23:46:45 +0800

    lecture 6 未完成

---
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -0,0 +1,23 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by 田万里 on 2019/6/2.
+//  Copyright © 2019 Wiley. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+        
+    }
+ 
+
+}
