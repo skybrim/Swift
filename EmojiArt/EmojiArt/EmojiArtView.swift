2d34fdd4e53c558ee03fcf118c355f30a6fe9537
Wiley <throughskybrim@gmail.com>
2019-06-15 22:05:03 +0800

    lecture 11 - tableview

---
+++ EmojiArt/EmojiArt/EmojiArtView.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiArtView.swift
+//  EmojiArt
+//
+//  Created by wiley on 2019/6/15.
+//  Copyright © 2019 wiley. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by wiley on 2019/6/15.
+//  Copyright © 2019 wiley. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var dropZone: UIView!
+    
+    @IBOutlet weak var emojiArtView: EmojiArtView!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
