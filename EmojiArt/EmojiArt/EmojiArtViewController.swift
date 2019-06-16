8d4c6dd5e94747489afb5222f56d8b5ffb76fd06
Wiley <throughskybrim@gmail.com>
2019-06-16 11:34:43 +0800

    lecture 11 UIDropInteraction

--- EmojiArt/EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,9 +16,53 @@ class EmojiArtViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var dropZone: UIView!
+    @IBOutlet weak var dropZone: UIView! {
+        didSet {
+            dropZone.addInteraction(UIDropInteraction(delegate: self))
+        }
+    }
     
+    //MARK: - UIDropInteractionDelegate
+    //这个方法返回是否响应此放置目的地的放置请求
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        
+        return true
+    }
+    //设置以何种方式响应拖放会话行为
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    var imageFetcher: ImageFetcher!
+    
+    //已经应用拖放行为后执行的操作
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        imageFetcher = ImageFetcher(handler: { (url, image) in
+            DispatchQueue.main.async {
+                self.emojiArtView.backgroundImage = image
+            }
+        })
+        
+        session.loadObjects(ofClass: NSURL.self) { (nsurls) in
+            if let url = nsurls.first as? URL {
+                self.imageFetcher.fetch(url)
+            }
+        }
+        
+        session.loadObjects(ofClass: UIImage.self) { (images) in
+            if let image = images.first as? UIImage {
+                self.imageFetcher.backup = image
+            }
+        }
+    }
+
+    
+    //MARK: -
     @IBOutlet weak var emojiArtView: EmojiArtView!
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
