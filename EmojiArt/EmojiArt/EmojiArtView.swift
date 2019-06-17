db5cf8a312269ab8a41977c36aff04574e99b734
Wiley <throughskybrim@gmail.com>
2019-06-18 00:09:29 +0800

    #

--- EmojiArt/EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArt/EmojiArtView.swift
@@ -8,9 +8,48 @@
 
 import UIKit
 
-class EmojiArtView: UIView {
+class EmojiArtView: UIView, UIDropInteractionDelegate {
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+    
+    func setUp() {
+        addInteraction(UIDropInteraction(delegate: self))
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: NSAttributedString.self) { (providers) in
+            let dropPoint = session.location(in: self)
+            for attributeString in providers as? [NSAttributedString] ?? [] {
+                self.addLabel(attributeString: attributeString, center: dropPoint)
+            }
+        }
+    }
+    
+    private func addLabel(attributeString: NSAttributedString, center: CGPoint) {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.attributedText = attributeString
+        addEmojiArtGestureRecognizers(to: label)
+        label.sizeToFit()
+        label.center = center
+        addSubview(label)
+    }
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
